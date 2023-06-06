ddfa45ff908b4c898cb4e1155e32c6978a4b67bd
Diegton Rodrigues <diegton@gmail.com>
2023-06-06 18:57:15 -0300

    dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,30 +1,32 @@
 class DashboardController < ApplicationController
+  @@colors = ["#5c5c8f", "#c690cd", "#fcc468", "#6bd098", "#f17e5d", "#ffc3c8", "#5390a7", "#7ebd5e", "#fdff79", "#d13737"]
+
   def index
-    @dashboard = {
-      labels: [1,1,2,2],
-      datast: [
+    analysis = Analysis.find_by({status: "active"})
+    if analysis
+      dates = analysis.analysis_dates
+      datas = analysis.lists.each_with_index.map do |list, index|
+        values = dates.map do |date|
+          date.date_values.find{ |date_value| date_value.list == list }
+        end
+        values = values.reject { |value| value == nil }
         {
-          label: 'My First dataset',
-          data: [3, 3, 3, 3],
-          borderColor: "#6bd098",
-          backgroundColor: "#6bd098",
-          fill: true
-        },
-        {
-          label: 'My Second dataset',
-          data: [3, 3, 3, nil],
-          borderColor: "#f17e5d",
-          backgroundColor: "#f17e5d",
-          fill: true
-        },
-        {
-          label: 'My Third dataset',
-          data: [3, 3, 3, nil],
-          borderColor: "#fcc468",
-          backgroundColor: "#fcc468",
+          label: list.name,
+          data: values.map {|value| value.value},
+          borderColor: @@colors[index],
+          backgroundColor: @@colors[index],
           fill: true
         }
-      ]
-    }
+      end
+      @dashboard = {
+        labels: dates.map {|date| date.date},
+        datast: datas
+      }
+    else
+      @dashboard = {
+        labels: [],
+        datast: []
+      }
+    end
   end
 end
